6cb34ffcb71c6a5b74cca81b1e6ff92513662dbc
Siphiwe Cilo <1124513@students.wits.ac.za>
2020-10-04 02:52:18 +0200

    serving js files

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,6 +1,15 @@
 from app import app
 from flask import render_template
+from flask import jsonify
 
 @app.route("/admin/dashboard")
 def admin_dashboard():
     return render_template("admin/dashboard.html")
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    img_data = await request.form()
+    img_bytes = await (img_data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    prediction = learn.predict(img)[0]
+    return jsonify({'result': str(prediction)})
