31b50f83c9b6c97d00f10641bb307277f155ed4c
Siphiwe Cilo <1124513@students.wits.ac.za>
2020-10-04 15:29:59 +0200

    app not analyzing

--- app/admin_views.py
+++ app/admin_views.py
@@ -11,5 +11,5 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
+    prediction = model.predict(img)[0]
     return jsonify({'result': str(prediction)})
